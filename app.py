2daa46197a9eb75e4482ee7b195c7190ccc9a269
Всеволод <63463168+htmlcssphpjs@users.noreply.github.com>
2021-06-01 21:58:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,85 +1,54 @@
 import strawberry
+from strawberry.flask.views import GraphQLView
 from typing import Optional, List
-import uvicorn, json, hashlib, os
-from fastapi import FastAPI
-from starlette.graphql import GraphQLApp
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import HTMLResponse, JSONResponse
+import hashlib, os
+from flask import Flask, render_template, request, redirect, jsonify
 
 from models import db_session
 from models.users import User
 
+app = Flask(__name__)
+
 db_session.global_init('database.db')
-
-app = FastAPI()
-
-#app.add_middleware(
-#    #CORSMiddleware,
-#    #allow_origins=settings.ALLOWED_HOSTS,
-#    allow_credentials=True,
-#    allow_methods=["*"],
-#    allow_headers=["*"],
-#)
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>API</title>
-    </head>
-    <body>
-        <h1><a href="/api">API</a></h1>
-    </body>
-</html>
-"""
 
 def hash_password(password):
     h = hashlib.md5(password.encode())
     return h.hexdigest()
 
-
 @strawberry.type
 class UsersType:
-  id: str
-  name: str
-  fullname:str
-  username:str
-  mail:str
-  hashed_password:str
-  resume: Optional[str]
+    id: str
+    name: str
+    pitch: str
+    picture: str
+    skills: Optional[str]
+    roadmap: Optional[str]
+    email: str
+    hashed_password: str
 
-#class CreateUser:
-#
-#    class Arguments:
-#        id = ID(required=True)
-#        name = String(required=True)
-#        fullname = String(required=True)
-#        username = String(required=True)
-#        mail = String(required=True)
-#        hashed_password = String(required=True)
-#        resume = String()
-#
-#    def mutate(self, info, id, name, fullname, username, mail, hashed_password, resume):
+#@strawberry.type
+#class Mutation:
+#    @strawberry.field
+#    def mutate(self, info, id: str, name: str, pitch: str, picture: str, skills: Optional[str], roadmap: Optional[str], email: str, hashed_password: str) -> Create:
 #        session = db_session.create_session()
 #        Create = []
 #
 #        user = User(
 #            id=id,
 #            name=name,
-#            fullname=fullname,
-#            username=username,
-#            mail=mail,
-#            hashed_password=hash_password(hashed_password),
-#            resume=resume
+#            pitch=pitch,
+#            picture=picture,
+#            skills=skills,
+#            roadmap=roadmap,
+#            email=email,
+#            hashed_password=hash_password(hashed_password)
 #		)
 #        session.add(user)
 #        session.commit()
-#        Create.append({"error": "None", "username": username})
+#        Create.append({"error": "None", "id": id})
 #        
-#        return Create[-1]
+#        return Create
 
-#class Mutation:
-#    create_user = CreateUser.Field()
 
 @strawberry.type
 class Query:
@@ -95,11 +64,12 @@ class Query:
                     user_as_dict = User(
                         id=user.id,
                         name=user.name,
-                        fullname=user.fullname,
-                        username=user.username,
-                        mail=user.mail,
-                        hashed_password=user.hashed_password,
-                        resume=user.resume
+                        pitch=user.pitch,
+                        picture=user.picture,
+                        skills=user.skills,
+                        roadmap=user.roadmap,
+                        email=user.email,
+                        hashed_password=user.hashed_password
                     )
                     users_list.append(user_as_dict)
                     break
@@ -107,34 +77,27 @@ class Query:
                 user_as_dict = User(
                     id=user.id,
                     name=user.name,
-                    fullname=user.fullname,
-                    username=user.username,
-                    mail=user.mail,
-                    hashed_password=user.hashed_password,
-                    resume=user.resume
+                    pitch=user.pitch,
+                    picture=user.picture,
+                    skills=user.skills,
+                    roadmap=user.roadmap,
+                    email=user.email,
+                    hashed_password=user.hashed_password
                 )
                 users_list.append(user_as_dict)
 
         print(users_list)
         return users_list
 
-@app.get("/")
-async def main():
-    return HTMLResponse(html)
 
-#app.add_route("/api", GraphQLApp(
-#    schema=Schema(query=Query))
-#)
-
-app.add_route(
-    "/api", GraphQLApp(
-        schema=strawberry.Schema(
-            query=Query, 
-            #mutation=Mutation
-        )
-    )
+schema = strawberry.Schema(query=Query)
+app.add_url_rule(
+    "/graphql",
+    view_func=GraphQLView.as_view(
+        "graphql_view", schema=schema, #mutation=Mutation
+    ),
 )
 
-#if __name__ == '__main__':
-    #port = int(os.environ.get("PORT", 5000))
-    #uvicorn.run(app, host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
